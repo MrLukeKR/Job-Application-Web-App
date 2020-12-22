ff1039914d3589bf83d16d0d4842ed98c14b06e0
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2020-12-22 20:46:22 +0000

    Added DataAnnotation based validation to entities

--- API/Entities/Address.cs
+++ API/Entities/Address.cs
@@ -1,13 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities
 {
     public class Address
     {
+        [Required]
         public int ID { get; set; }
+
+        [Required]
         public string Address1 { get; set; }
         public string Address2 { get; set; }
         public string Address3 { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         public string Town { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         public string County { get; set; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
         public string Postcode { get; set; }
     }
 }
--- API/Entities/Applicant.cs
+++ API/Entities/Applicant.cs
@@ -1,16 +1,39 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Entities
 {
     public class Applicant
     {
+        [Required]
         public int ID { get; set; }
+        
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Forename must have a length between 2 and 30 characters")]
+        [DataType(DataType.Text)]
         public string Forename { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Surname must have a length between 2 and 30 characters")]
+        [DataType(DataType.Text)]
         public string Surname { get; set; }
+        
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+        
+        [Required]
         public Address HomeAddress { get; set; }
-        public string MobilePhone { get; set; }
+        
+        [Required]
+        [DataType(DataType.PhoneNumber)]
         public string HomePhone{ get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string MobilePhone { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
     }
 }
