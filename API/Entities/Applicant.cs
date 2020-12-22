a6bb0b44893ce2a028918ba65a995e036b755f49
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2020-12-22 17:48:47 +0000

    Added SQLite to app

---
+++ API/Entities/Applicant.cs
@@ -0,0 +1,9 @@
+namespace API.Entities
+{
+    public class Applicant
+    {
+        public int ID { get; set; }
+        private string Forename { get; set; }
+        private string Surname { get; set; }
+    }
+}
