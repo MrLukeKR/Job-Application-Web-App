90b6a3f2eeddeb2a290fec3c12368aaa4aca467e
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2020-12-22 23:05:26 +0000

    Added CORS

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientJsonpModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'ACME CORP - Job Application';
+  applicants: any;
+
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit() {
+    this.getApplicants();
+  }
+
+  // Retrieve the database contents for applicant information from the ASP.NET API
+  getApplicants() {
+    this.http.get("https://localhost:5001/api/applicants").subscribe(response => 
+    { this.applicants = response; }, 
+    error => { console.log(error); }
+    );
+  }
 }
