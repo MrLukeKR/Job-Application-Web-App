503629e6b76c6891a663e0ab8de9084ee31c8eb7
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2021-01-03 14:41:35 +0000

    Migrated data validation from entity classes to DTO

--- API/DTOs/ApplicationDTO.cs
+++ API/DTOs/ApplicationDTO.cs
@@ -1,22 +1,54 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
     public class ApplicationDTO
     {
+        [Required]
+        [DataType(DataType.Text)]
         public string Forename{ get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         public string Surname { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         public string address1 { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         public string address2 { get; set; }
+
+        [DataType(DataType.Text)]
         public string address3 { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         public string county { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         public string town { get; set; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
         public string postcode { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
         public string HomePhone { get; set; }
+        
+        [DataType(DataType.PhoneNumber)]
         public string MobilePhone { get; set; }
-        public DateTime StartDate { get; set; }
-                //address: Address;   
-                
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }               
     }
 }
--- API/Entities/Address.cs
+++ API/Entities/Address.cs
@@ -4,29 +4,12 @@ namespace API.Entities
 {
     public class Address
     {
-        [Required]
         public int ID { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
         public string Address1 { get; set; }
-        
-        [DataType(DataType.Text)]
         public string Address2 { get; set; }
-        
-        [DataType(DataType.Text)]
         public string Address3 { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
         public string Town { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
         public string County { get; set; }
-
-        [Required]
-        [DataType(DataType.PostalCode)]
         public string Postcode { get; set; }
     }
 }
