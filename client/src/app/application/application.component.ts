60ce0aee1ee5f49928709637242597cb14570e10
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2021-01-03 17:53:00 +0000

    Input form now has validation

--- client/src/app/application/application.component.ts
+++ client/src/app/application/application.component.ts
@@ -1,5 +1,7 @@
+import { formatDate } from "@angular/common";
+import { analyzeAndValidateNgModules } from "@angular/compiler";
 import { Component, OnInit, Output } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { EventEmitter } from "events";
 
 @Component({
@@ -11,25 +13,27 @@ export class ApplicationComponent implements OnInit{
     @Output() cancelApplication = new EventEmitter();
     model: any = {};
     applicationForm: FormGroup;
+    today: any;
 
     ngOnInit(): void{
         this.initForm();
+        this.today = formatDate(new Date(), 'yyyy-MM-dd', 'en');
     }
 
     initForm(){
         this.applicationForm = new FormGroup({
-            forename: new FormControl,
-            surname: new FormControl(),
-            emailAddress: new FormControl(),
-            address1: new FormControl(),
-            address2: new FormControl(),
+            forename: new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
+            surname: new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
+            emailAddress: new FormControl("", [Validators.required, Validators.email]),
+            address1: new FormControl("", Validators.required),
+            address2: new FormControl("", Validators.required),
             address3: new FormControl(),
-            town: new FormControl(),
-            county: new FormControl(),
-            postcode: new FormControl(),
-            mobilePhone: new FormControl(),
-            homePhone: new FormControl(),
-            startDate: new FormControl()
+            town: new FormControl("", Validators.required),
+            county: new FormControl("", Validators.required),
+            postcode: new FormControl("", [Validators.required, Validators.pattern("[a-z]{1,2}[0-9][a-z0-9]?\s?[0-9][a-z]{2}")]),
+            homePhone: new FormControl("", [Validators.required, Validators.minLength(10), Validators.maxLength(11), Validators.pattern("[0-9]")]),
+            mobilePhone: new FormControl("", [Validators.minLength(10), Validators.maxLength(11), Validators.pattern("[0-9]")]),
+            startDate: new FormControl("", Validators.required)
         });
     }
 
